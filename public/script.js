1aa5d77dc5eebc361e050182b57a0569cbe5ff3d
yusufrijalr14 <yusufrijalr14@gmail.com>
2022-12-13 14:44:23 +0700

    updated camera

--- public/script.js
+++ public/script.js
@@ -36,60 +36,60 @@ const constraints = {
       max: 1440
     },
     facingMode: {
-      exact: 'user'
+      exact: 'environment'
     }
   }
 };
 
-// depan.onclick = () => {
-//   frame.src = "frame/innova/depan.png";
-//   $('#exampleModalCenter').modal('hide');
-// };
+depan.onclick = () => {
+  frame.src = "frame/innova/depan.png";
+  $('#exampleModalCenter').modal('hide');
+};
 
-// belakang.onclick = () => {
-//   frame.src = "frame/innova/belakang.png";
-//   $('#exampleModalCenter').modal('hide');
-// };
+belakang.onclick = () => {
+  frame.src = "frame/innova/belakang.png";
+  $('#exampleModalCenter').modal('hide');
+};
 
-// sampingKananDepan.onclick = () => {
-//   frame.src = "frame/innova/samping-kanan-depan.png";
-//   $('#exampleModalCenter').modal('hide');
-// };
+sampingKananDepan.onclick = () => {
+  frame.src = "frame/innova/samping-kanan-depan.png";
+  $('#exampleModalCenter').modal('hide');
+};
 
-// sampingKiriDepan.onclick = () => {
-//   frame.src = "frame/innova/samping-kiri-depan.png";
-//   $('#exampleModalCenter').modal('hide');
-// };
+sampingKiriDepan.onclick = () => {
+  frame.src = "frame/innova/samping-kiri-depan.png";
+  $('#exampleModalCenter').modal('hide');
+};
 
-// sampingKananBelakang.onclick = () => {
-//   frame.src = "frame/innova/samping-kanan-belakang.png";
-//   $('#exampleModalCenter').modal('hide');
-// };
+sampingKananBelakang.onclick = () => {
+  frame.src = "frame/innova/samping-kanan-belakang.png";
+  $('#exampleModalCenter').modal('hide');
+};
 
-// sampingKiriBelakang.onclick = () => {
-//   frame.src = "frame/innova/samping-kiri-belakang.png";
-//   $('#exampleModalCenter').modal('hide');
-// };
+sampingKiriBelakang.onclick = () => {
+  frame.src = "frame/innova/samping-kiri-belakang.png";
+  $('#exampleModalCenter').modal('hide');
+};
 
-// lampuDepanKanan.onclick = () => {
-//   frame.src = "frame/innova/lampu-depan-kanan.png";
-//   $('#exampleModalCenter').modal('hide');
-// };
+lampuDepanKanan.onclick = () => {
+  frame.src = "frame/innova/lampu-depan-kanan.png";
+  $('#exampleModalCenter').modal('hide');
+};
 
-// lampuDepanKiri.onclick = () => {
-//   frame.src = "frame/innova/lampu-depan-kiri.png";
-//   $('#exampleModalCenter').modal('hide');
-// };
+lampuDepanKiri.onclick = () => {
+  frame.src = "frame/innova/lampu-depan-kiri.png";
+  $('#exampleModalCenter').modal('hide');
+};
 
-// lampuBelakangKanan.onclick = () => {
-//   frame.src = "frame/innova/lampu-belakang-kanan.png";
-//   $('#exampleModalCenter').modal('hide');
-// };
+lampuBelakangKanan.onclick = () => {
+  frame.src = "frame/innova/lampu-belakang-kanan.png";
+  $('#exampleModalCenter').modal('hide');
+};
 
-// lampuBelakangKiri.onclick = () => {
-//   frame.src = "frame/innova/lampu-belakang-kiri.png";
-//   $('#exampleModalCenter').modal('hide');
-// };
+lampuBelakangKiri.onclick = () => {
+  frame.src = "frame/innova/lampu-belakang-kiri.png";
+  $('#exampleModalCenter').modal('hide');
+};
 
 play.onclick = () => {
   if (streamStarted) {
