c8fd65ab6d908575e7b1c5b07592c8b19ffb4725
yusufrijalr14 <yusufrijalr14@gmail.com>
2022-12-19 16:05:15 +0700

    updated camera

--- public/assets-voxo/script.js
+++ public/assets-voxo/script.js
@@ -49,40 +49,46 @@ feedback.onclick = () => {
     window.location.href = 'feedback';
 };
 
-const constraints = {
-    video: {
-        width: {
-            min: 1280,
-            ideal: 1920,
-            max: 2560,
-        },
-        height: {
-            min: 720,
-            ideal: 1080,
-            max: 1440
-        }
-    }
-};
+// const constraints = {
+//     video: {
+//         width: {
+//             min: 1280,
+//             ideal: 1920,
+//             max: 2560,
+//         },
+//         height: {
+//             min: 720,
+//             ideal: 1080,
+//             max: 1440
+//         }
+//     }
+// };
 
-const startStream = async () => {
-    if ('mediaDevices' in navigator && navigator.mediaDevices.getUserMedia) {
-        const updatedConstraints = {
-            ...constraints
-        };
+// const startStream = async () => {
+//     if ('mediaDevices' in navigator && navigator.mediaDevices.getUserMedia) {
+//         const updatedConstraints = {
+//             ...constraints
+//         };
 
-        await navigator.mediaDevices.getUserMedia(updatedConstraints);
-    }
+//         await navigator.mediaDevices.getUserMedia(updatedConstraints);
+//     }
 
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(showPosition);
-    } else { 
-        console.log("Geolocation is not supported by this browser.");
-    }
+//     if (navigator.geolocation) {
+//         navigator.geolocation.getCurrentPosition(showPosition);
+//     } else { 
+//         console.log("Geolocation is not supported by this browser.");
+//     }
 
-    function showPosition(position) {
-        var latitude = position.coords.latitude;
-        var longitude = position.coords.longitude;
-    }
-};
+//     function showPosition(position) {
+//         var latitude = position.coords.latitude;
+//         var longitude = position.coords.longitude;
+//     }
+// };
 
-startStream();
+// startStream();
+
+function testing() {
+    return navigator.mediaDevices.getUserMedia && navigator.geolocation;
+}
+
+testing();
