82b2a6d3cd38675969d11a0d1546049c03afeaa7
yusufrijalr14 <yusufrijalr14@gmail.com>
2022-12-29 17:04:23 +0700

    updated

--- app/Http/Controllers/CameraController.php
+++ app/Http/Controllers/CameraController.php
@@ -34,7 +34,6 @@ class CameraController extends Controller
 
     public function store(Request $request)
     {
-        return $request->image;
         $userCode = Cookie::get('user_code');
         $token = Cookie::get('auth_token');
         
--- public/script.js
+++ public/script.js
@@ -140,7 +140,7 @@ const constraints = {
             max: 1440
         },
         facingMode: {
-            exact: 'user'
+            exact: 'environment'
         }
     }
 };
