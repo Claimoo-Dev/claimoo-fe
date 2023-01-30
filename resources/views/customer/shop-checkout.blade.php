294494863329efa1787ab39583f1a5ee8fb82112
yusufrijalr14 <yusufrijalr14@gmail.com>
2023-01-30 10:54:57 +0700

    added shop page

---
+++ resources/views/customer/shop-checkout.blade.php
@@ -0,0 +1,402 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <link rel="manifest" href="./manifest.json" />
+    <link rel="icon" href="assets-voxo/images/favicon/favicon.png" type="image/x-icon" />
+    <link rel="apple-touch-icon" href="assets-voxo/images/favicon/favicon.png" />
+    <meta name="theme-color" content="#073f78" />
+    <meta name="apple-mobile-web-app-capable" content="yes" />
+    <meta name="apple-mobile-web-app-status-bar-style" content="black" />
+    <meta name="apple-mobile-web-app-title" content="Claimoo" />
+    <meta name="msapplication-TileImage" content="assets-voxo/images/favicon/favicon.png" />
+    <meta name="msapplication-TileColor" content="#FFFFFF" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Claimoo">
+    <meta name="keywords" content="Claimoo">
+    <meta name="author" content="Claimoo">
+    <link rel="icon" href="assets/img/favicon.png" type="image/x-icon" />
+    <title>Claimoo | Beli Polis</title>
+
+    <!-- Google font -->
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Rubik:wght@400;500;600;700&display=swap" rel="stylesheet">
+
+    <!-- bootstrap css -->
+    <link id="rtl-link" rel="stylesheet" type="text/css" href="assets-voxo/css/vendors/bootstrap.css">
+
+    <!-- font-awesome css -->
+    <link rel="stylesheet" type="text/css" href="assets-voxo/css/vendors/font-awesome.css">
+
+    <!-- feather icon css -->
+    <link rel="stylesheet" type="text/css" href="assets-voxo/css/vendors/feather-icon.css">
+
+    <!-- animation css -->
+    <link rel="stylesheet" type="text/css" href="assets-voxo/css/vendors/animate.css">
+
+    <!-- slick css -->
+    <link rel="stylesheet" type="text/css" href="assets-voxo/css/vendors/slick/slick.css">
+    <link rel="stylesheet" type="text/css" href="assets-voxo/css/vendors/slick/slick-theme.css">
+
+    <!-- select2 -->
+    <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
+
+    <!-- Theme css -->
+    <link id="color-link" rel="stylesheet" type="text/css" href="assets-voxo/css/demo2.css">
+    <link id="color-link" rel="stylesheet" type="text/css" href="assets-voxo/css/custom-style.css">
+
+</head>
+
+<body class="theme-color2 light ltr">
+    <!-- header start -->
+    <header class="header-style-2" id="home">
+        <div class="main-header navbar-searchbar">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="main-menu">
+                            <div class="menu-left">
+                                <div class="brand-logo">
+                                    <a href="{{ route('landing-page') }}">
+                                        <img src="assets-voxo/images/logo-claimoo.png"
+                                            class="img-fluid blur-up lazyload" alt="logo">
+                                    </a>
+                                </div>
+                            </div>
+                            <div class="menu-right">
+                                <ul>
+                                    <li class="onhover-dropdown wislist-dropdown">
+                                        <div class="cart-media">
+                                            <div class="cart-icon">
+                                                <i data-feather="bell"></i>
+                                            </div>
+                                        </div>
+                                    </li>
+
+                                    <li class="modal-icon">
+                                        <div class="modal-nav" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                                            <i data-feather="align-justify" class="sidebar-bar"></i>
+                                        </div>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+    <!-- header end -->
+
+    <!-- mobile fix menu start -->
+    <div class="mobile-menu">
+        <ul>
+            <li>
+                <a href="{{ route('landing-page') }}">
+                    <i data-feather="home"></i>
+                    <span>Beranda</span>
+                </a>
+            </li>
+            <li>
+                <a href="{{ route('shop') }}">
+                    <i data-feather="shopping-bag"></i>
+                    <span>Belanja</span>
+                </a>
+            </li>
+            <li>
+                <a href="{{ route('claim') }}">
+                    <i color="red" data-feather="edit"></i>
+                    <span>Klaim</span>
+                </a>
+            </li>
+            <li>
+                <a href="{{ route('support') }}">
+                    <i data-feather="help-circle"></i>
+                    <span>Bantuan</span>
+                </a>
+            </li>
+            <li>
+                <a href="{{ route('dashboard-customer') }}">
+                    <i data-feather="user"></i>
+                    <span>Akun</span>
+                </a>
+            </li>
+        </ul>
+    </div>
+    <!-- mobile fix menu end -->
+
+    <div class="content-container">
+        
+    </div>
+
+    <footer class="footer-sm-space">
+        <div class="main-footer">
+            <div class="container">
+                <div class="row gy-4">
+                    <div class="col-12">
+                        <div class="footer-contact">
+                            <div class="brand-logo">
+                                <a href="{{ route('landing-page') }}" class="footer-logo">
+                                    <img src="assets-voxo/images/logo-claimoo.png" class="img-fluid blur-up lazyload"
+                                        alt="logo">
+                                </a>
+                            </div>
+                            <p class="font-light">Jl. Tole Iskandar No. 66, Depok, Jawa Barat 16412
+                            </p>
+                            <!-- <p>Download app from:</p>
+                            <div>
+                                <center><img src="assets-voxo/images/logos/android.png" width="100">
+                                    <img src="assets-voxo/images/logos/ios.png" width="100">
+                                </center>
+                            </div> -->
+                        </div>
+                    </div>
+
+                    <div class="col-12">
+                        <div class="footer-links">
+                            <div class="footer-title">
+                                <h3>Tentang Kami</h3>
+                            </div>
+                            <div class="footer-content">
+                                <ul>
+                                    <li>
+                                        <a href="javascript:void(0)" class="font-dark">Beranda</a>
+                                    </li>
+                                    <li>
+                                        <a href="javascript:void(0)" class="font-dark">Perusahaan</a>
+                                    </li>
+                                    <li>
+                                        <a href="javascript:void(0)" class="font-dark">Komisaris &amp; Direksi</a>
+                                    </li>
+                                    <li>
+                                        <a href="javascript:void(0)" class="font-dark">Tenaga Ahli</a>
+                                    </li>
+                                    <li>
+                                        <a href="javascript:void(0)" class="font-dark">Partner</a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-12">
+                        <div class="footer-links">
+                            <div class="footer-title">
+                                <h3>Produk Asuransi</h3>
+                            </div>
+                            <div class="footer-content">
+                                <ul>
+                                    <li>
+                                        <a href=javascript:void(0)" class="font-dark">Simas Mobil</a>
+                                    </li>
+                                    <li>
+                                        <a href="javascript:void(0)" class="font-dark">Zurich Autocilin</a>
+                                    </li>
+                                    <li>
+                                        <a href="javascript:void(0)" class="font-dark">Astra Garda Oto</a>
+                                    </li>
+
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-12">
+                        <div class="footer-links">
+                            <div class="footer-title">
+                                <h3>Bantuan</h3>
+                            </div>
+                            <div class="footer-content">
+                                <ul>
+                                    <li>
+                                        <a href="javascript:void(0)" class="font-dark">Akun Anda</a>
+                                    </li>
+                                    <li>
+                                        <a href="javascript:void(0)" class="font-dark">Keranjang</a>
+                                    </li>
+                                    <li>
+                                        <a href="javascript:void(0)" class="font-dark">FAQs</a>
+                                    </li>
+                                    <li>
+                                        <a href="javascript:void(0)" class="font-dark">Kebijakan Privasi</a>
+                                    </li>
+                                    <li>
+                                        <a href="javascript:void(0)" class="font-dark">Hubungi Kami</a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+
+                    <!-- <div class="col-xl-3 col-lg-4 col-sm-6 d-none d-sm-block">
+                        <div class="footer-newsletter">
+                            <h3>Hubungi kami:</h3>
+                            <div class="footer-number">
+                                <div class="footer-number-image">
+                                    <img src="assets-voxo/images/shoes/other/headphone.png"
+                                        class="img-fluid blur-up lazyload" alt="">
+                                </div>
+
+                                <div class="footer-number-container">
+                                    <h3>+62 21 7783 7035</h3>
+                                </div>
+                            </div>
+
+                            <div class="footer-details">
+                                <ul>
+                                    <li>
+                                        <span>
+                                            <b>Alamat: </b><span class="font-light">Jl. Tole Iskandar No. 66, Depok.
+                                                Jawa Barat. 16412</span>
+                                        </span>
+                                    </li>
+                                    <li>
+                                        <span>
+                                            <b>Email: </b><span class="font-light"> admin@claimoo.com</span>
+                                        </span>
+                                    </li>
+                                </ul>
+
+                                <div>
+                                    <img src="assets-voxo/images/logos/ojk.webp" width="200">
+                                </div>
+                            </div>
+                        </div>
+                    </div> -->
+                </div>
+            </div>
+        </div>
+        <div class="sub-footer">
+            <div class="container">
+                <div class="row gy-3">
+                    <div class="col-md-12">
+                        <p class="mb-0 font-dark text-center">© 2023 Claimoo. Hak Cipta Dilindungi Undang-undang</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+
+    <div class="modal fade modal-navbar" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel"
+        aria-hidden="true">
+        <div class="modal-dialog modal-fullscreen">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <div class="menu">
+                        <a href="#">Beranda</a>
+                    </div>
+                    <div class="menu">
+                        <div class="dropdown">
+                            <a data-bs-toggle="collapse" href="#collapseExample" role="button" aria-expanded="false"
+                                aria-controls="collapseExample">
+                                Tentang Kami
+                            </a>
+
+                            <div class="collapse" id="collapseExample">
+                                <ul class="menu-child">
+                                    <a href="">
+                                        <li class="menu-child-item"> Perusahaan </li>
+                                    </a>
+                                    <a href="">
+                                        <li class="menu-child-item"> Komisaris & Direksi </li>
+                                    </a>
+                                    <a href="">
+                                        <li class="menu-child-item"> Tenaga Ahli </li>
+                                    </a>
+                                    <a href="">
+                                        <li class="menu-child-item"> Partner </li>
+                                    </a>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="menu">
+                        <div class="dropdown">
+                            <a data-bs-toggle="collapse" href="#collapseExample1" role="button" aria-expanded="false"
+                                aria-controls="collapseExample1">
+                                Produk Asuransi
+                            </a>
+
+                            <div class="collapse" id="collapseExample1">
+                                <ul class="menu-child">
+                                    <a href="">
+                                        <li class="menu-child-item"> All Risk </li>
+                                    </a>
+                                    <a href="">
+                                        <li class="menu-child-item"> TLO </li>
+                                    </a>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="menu">
+                        <a href="#">Bengkel</a>
+                    </div>
+                    <div class="menu">
+                        <a href="#">Berita</a>
+                    </div>
+                    <div class="menu">
+                        <a href="#">Hubungi Kami</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- tap to top Section Start -->
+    <div class="tap-to-top">
+        <a href="#home">
+            <i class="fas fa-chevron-up"></i>
+        </a>
+    </div>
+    <!-- tap to top Section End -->
+
+    <div class="bg-overlay"></div>
+
+    <!-- latest jquery-->
+    <script src="assets-voxo/js/jquery-3.5.1.min.js"></script>
+
+    <!-- Add To Home js -->
+    <script src="assets-voxo/js/pwa.js"></script>
+
+    <!-- Bootstrap js-->
+    <script src="assets-voxo/js/bootstrap/bootstrap.bundle.min.js"></script>
+
+    <!-- feather icon js-->
+    <script src="assets-voxo/js/feather/feather.min.js"></script>
+
+    <!-- lazyload js-->
+    <script src="assets-voxo/js/lazysizes.min.js"></script>
+
+    <!-- Slick js-->
+    <script src="assets-voxo/js/slick/slick.js"></script>
+    <script src="assets-voxo/js/slick/slick-animation.min.js"></script>
+    <script src="assets-voxo/js/slick/custom_slick.js"></script>
+
+    <!-- Filter Hide and show Js -->
+    <script src="assets-voxo/js/filter.js"></script>
+
+    <!-- Notify js-->
+    <script src="assets-voxo/js/bootstrap/bootstrap-notify.min.js"></script>
+
+    <!-- script js -->
+    <script src="assets-voxo/js/theme-setting.js"></script>
+    <script src="assets-voxo/js/script.js"></script>
+
+    <!-- select2 -->
+    <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
+
+</body>
+
+</html>
