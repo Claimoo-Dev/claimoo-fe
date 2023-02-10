19a1ba678388f08b73df2104c79aa630d76ab44e
yusufrijalr14 <yusufrijalr14@gmail.com>
2023-02-10 15:35:45 +0700

    updated

--- public/assets-voxo/js/custom-script.js
+++ public/assets-voxo/js/custom-script.js
@@ -13,30 +13,18 @@ function formatNumber(number) {
 }
 
 function formatRupiah(angka) {
-    // var number_string = angka.replace(/[^,\d]/g, "").toString(),
-    //     split = number_string.split(","),
-    //     sisa = split[0].length % 3,
-    //     rupiah = split[0].substr(0, sisa),
-    //     ribuan = split[0].substr(sisa).match(/\d{3}/gi);
+    var number_string = angka.replace(/[^,\d]/g, "").toString(),
+        split = number_string.split(","),
+        sisa = split[0].length % 3,
+        rupiah = split[0].substr(0, sisa),
+        ribuan = split[0].substr(sisa).match(/\d{3}/gi);
 
-    // // tambahkan titik jika yang di input sudah menjadi angka ribuan
-    // if (ribuan) {
-    //     separator = sisa ? "." : "";
-    //     rupiah += separator + ribuan.join(".");
-    // }
-
-    // rupiah = split[1] != undefined ? rupiah + "," + split[1] : rupiah;
-    // return rupiah;
-
-    var	number_string = angka.replace(/[^,\d]/g, "").toString(),
-        sisa 	= number_string.length % 3,
-        rupiah 	= number_string.substr(0, sisa),
-        ribuan 	= number_string.substr(sisa).match(/\d{3}/g);
-            
+    // tambahkan titik jika yang di input sudah menjadi angka ribuan
     if (ribuan) {
-        separator = sisa ? '.' : '';
-        rupiah += separator + ribuan.join('.');
+        separator = sisa ? "." : "";
+        rupiah += separator + ribuan.join(".");
     }
 
+    rupiah = split[1] != undefined ? rupiah + "," + split[1] : rupiah;
     return rupiah;
 }
